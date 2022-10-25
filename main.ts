17986d9fc89c381ef3d2f21e4a03bb6c7b302baa
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-10-25 10:49:14 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
+// backward
 input.onButtonPressed(Button.A, function () {
     pins.servoSetPulse(AnalogPin.P8, 1300)
     control.waitMicros(20000)
 })
+// stop
 input.onButtonPressed(Button.AB, function () {
     pins.servoSetPulse(AnalogPin.P8, 0)
     control.waitMicros(20000)
 })
+// forward
 input.onButtonPressed(Button.B, function () {
     pins.servoSetPulse(AnalogPin.P8, 1700)
     control.waitMicros(20000)
